037f6bb2283eea4a23640194bd0df7d253caa1fb
Kim Do Ky <makingfunk0@gmail.com>
2017-05-10 18:17:52 +0900

    study의 TDD를 새로운 repository으로 새로 시작함

---
+++ TDD/superlists/functional_tests/tests.py
@@ -0,0 +1,90 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import unittest
+
+from selenium.webdriver.common.keys import Keys
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 앱 사이트를 확인하러 간다
+        self.browser.get(self.live_server_url)
+
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 그녀는 바로 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # "공작깃털 사기"라고 텍스트 상자에 입력한다
+        # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
+        # 대시 "공작깃털을 이용해서 그물 만들기"라고 입력한다(에디스는 매우 체계적인 사람이다)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # 새로운 사용자인 프란시스가 사이트에 접속한다
+
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 프란시스가 홈페이지에 접속한다
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
+        # 그는 에디스보가 재미가 없다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #  둘 다 만족하고 잠자리에 든다

1261456bd032251f899fe5dcdcf9a74a1cf6e3e5
Kim Do Ky <makingfunk0@gmail.com>
2017-05-26 22:33:38 +0900

    ch 7.8 a URL for Adding List Items

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -33,7 +33,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)

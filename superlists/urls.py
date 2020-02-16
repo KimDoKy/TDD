2b212da6dfb3d1b68edc52a66b7f9d1a3da057c3
KimDoKy <makingfunk0@gmail.com>
2020-02-16 14:37:58 +0900

    다른 repo에서 chap8까지 완료한 파일을 TDD repo로 복사

---
+++ superlists/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from lists import views as list_views
+from lists import urls as list_urls
+
+urlpatterns = [
+        path('', list_views.home_page, name='home'),
+        path('lists/', include(list_urls)),
+]
